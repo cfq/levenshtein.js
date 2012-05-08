f4b88206b16a9f27fcf9f391d389b447a7c6197d
Mehmet Duran <mehmet.duran@gmail.com>
2012-05-08 02:54:09 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 levenshtein.js
 ==============
 
-Levenshtein Distance (or edit distance) calculator in JavaScript
+Levenshtein Distance (or edit distance) calculator in JavaScript. Implements the algorithm developed by [Robert A. Wagner and Michael J. Fischer](http://en.wikipedia.org/wiki/Levenshtein_distance).
---
+++ test/test-levenshtein.js
@@ -0,0 +1,52 @@
+function levenstein( first, second ){
+	var d	 = [],
+		flen = first.length,
+		slen = second.length;
+	
+	for( i = 0; i <= flen; i++ ){
+		d[i] = d[i] ? d[i] : [];
+		d[i][0] = i;
+	}
+	
+	for( j = 0; j <= slen; j++ ){
+		d[0][j] = d[0][j] ? d[0][j] : [];
+		d[0][j] = j;
+	}
+	
+	for( j = 1; j <= slen; j++ ){
+		for( i = 1; i <= flen; i++ ){
+			if( first[i-1] == second[j-1] ){
+				d[i][j] = d[i-1][j-1];
+			}else{
+				d[i][j] = Math.min( 
+					d[i-1][j]	+ 1, 
+					d[i][j-1]	+ 1, 
+					d[i-1][j-1] + 2 
+				);
+			}
+		}
+	}
+	
+	printMatrix(d, first, second);
+	
+	return d[flen][slen];
+}
+
+function printMatrix( d, f, s ){
+	var out = "";
+	
+	out = ["  ", "0"].concat(f.split('')).join(' | ') + " |\n";
+	
+	for( var i = 0; i < d.length; i++ ){
+		out += ( i == 0 ? " 0" :  ' ' + (s[i-1] ? s[i-1] : '-' )) + " |";
+		for( var j = 0; j < d[i].length; j++ ){
+			out += (isNaN(d[i][j]) ? -1 : ("  " + d[i][j]).slice(-2) ) + " |";
+		}
+		out += "\n";
+	}
+	
+	console.log(out);
+	console.log("\n");
+}
+
+console.log( "Levensten distance between 'javascript' and 'java': " + levenstein('javascript', 'java') );
